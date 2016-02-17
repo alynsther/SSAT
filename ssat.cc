b76266cf4d471c8f817d833be1a8e3a5557e823e
Son D. Ngo <sngo@bowdoin.edu>
2016-02-17 03:20:28 -0500

    a

--- ssat.cc
+++ ssat.cc
@@ -40,11 +40,25 @@
 using namespace std;
 
 /***************************************************************************/
+/* CONSTANTS */
+static const double CHOICE_VALUE = -1.0;
+static const int POSITIVE = 1;
+static const int NEGATIVE = -1;
+static const int INVALID = 0; 
+
+/***************************************************************************/
+
+/***************************************************************************/
 /* structs */
 typedef struct varInfo{
     double value;               // the value of the variables
     vector <int> clauseMembers;  // the indexes of the clauses the var is in        
 } varInfo;
+
+//Son's comment on varInfo: I think we should turn clauseMembers into a map
+//of <int, int> where the first one is the index of the clause and the value
+//indicates what its assignment is in that corresponding clause. This modification
+//would help a lot of with updating clauses when we assign a value to a variable
 
 /***************************************************************************/
 /* globals variables */
@@ -60,16 +74,22 @@ int numClauses;
 
 map <int, varInfo> variables;
 map <int, vector<int> > clauses;
+map <int, int> assignment;
 
 vector <vector <int> > solution;
+
 double successProbability;
 double solutionTime;
 
 /***************************************************************************/
 /* functions */
+double SOLVESSAT(clauses, chace-var-probabilities);
 void readFile(string input);
 void tokenize(string str, vector<string> &token_v);
-void isPureChoice();
+pair<bool, int> isPureChoice(int variable);
+int variableSignInClause(int clauseEntry);
+void updateSATclauses(int variable, int value);
+void assign(int variable, int value);
 
 /*****************************************************************************
  Function:  main
@@ -225,6 +245,40 @@ void readFile(string input) {
 }
 
 /***************************************************************************
+ Function:  SOLVESSAT
+ Inputs:    clauses, assignment, chance-var-probabilities
+ Returns:   double
+ Description:
+            THE MAIN ALGORITHM IMPLEMENTATION
+ ***************************************************************************/
+double SOLVESSAT(clauses, chace-var-probabilities){
+
+
+    //check for pure CHOICE variable
+
+    map<int, varInfo>::iterator it = variables.begin();
+
+    pair<bool, int> result;
+
+    int v = 0;
+    int value = 0;
+
+    for (map<int, varInfo>::iterator it = variables.begin(); it != variables.end(); it++){
+        v = it->first;
+        result = isPureChoice(it->first);
+        if (result.first == false){
+            continue;
+        }
+        else {
+            value = result.second;
+            assign(v, value);
+            updateSATclauses(v, value);
+        }
+    }
+
+}
+
+/***************************************************************************
  Function:  tokenize
  Inputs:    string and vector
  Returns:   vector
@@ -246,11 +300,104 @@ void tokenize(string str, vector<string> &token_v){
 
  
 /***************************************************************************
- Function:
- Inputs:
- Returns:
+ Function:  isPureChoice
+ Inputs:    int
+ Returns:   True if the variable is a pure choice variable
  Description:
  ***************************************************************************/
+pair<bool, int> isPureChoice(int variable) {
+    varInfo info = variables[variable];
+    if info.value != CHOICE_VALUE {
+        return false;
+    }
 
+    vector<int>* clauseVect = &(info.clauseMembers);
+
+    int clauseEntry = 0;
+    int currentStatus = POSITIVE;
+    bool switchSign = false;
+
+    for (unsigned int i = 0; i < (*clauseVect).size(); i++){
+        clauseEntry = (*clauseVect)[i];
+        if (currentStatus != variableSignInClause(clauseEntry, variable)){
+            if !(switchSign) {
+                currentStatus = NEGATIVE;
+                switchSign = true;
+            }
+            else {
+                return pair<bool, int>(false, INVALID);
+            }
+        } 
+    }
+
+    return pair<bool, int>(true, currentStatus);
+}
+
+/***************************************************************************
+ Function:  variableSignInClause
+ Inputs:    int
+ Returns:   int (positive or negative)
+ Description:   indicates the sign of a variable within a given clause
+ ***************************************************************************/
+int variableSignInClause(int clauseEntry, int variable) {
+
+    vector<int>* clause = clauses[clauseEntry];
+
+    bool foundSign = false;
+
+    for (unsigned int i = 0; i < (*clause).size(); i++){
+        if (abs((*clause)[i]) == variable) {
+            return (*clause[i] / variable);
+        } 
+    }
+
+    return INVALID;
+}
+
+/***************************************************************************
+ Function:  updateSATclauses
+ Inputs:    int
+ Returns:   int (positive or negative)
+ Description:   indicates the sign of a variable within a given clause
+ ***************************************************************************/
+void updateSATclauses(int variable, int value) {
+
+    varInfo info = variables[variable];
+
+    vector<int>& clauseVect = &(info.clauseMembers);
+
+    int clauseEntry = 0;
+
+    for (unsigned int i = 0; i < (*clauseVect).size(); i++){
+        clauseEntry = (*clauseVect)[i];
+
+        //clause is satisfied
+        if (variableSignInClause(clauseEntry, variable) == value){
+
+            vector<int>* clause = clauses[clauseEntry];
+
+            for (unsigned int i = 0; i < (*clause).size(); i++){
+                varInfo currInfo = variables[i];
+
+                //this should be much easier with a map! Otherwise I will have to
+                //go through the whole list
+                //need to erase the clause that is stored in the varInfo struct.vector/(map)
+                currInfo.clauseMembers.erase(currInfo.clauseMembers.begin() + variable);
+            }
+        }
+    }
+
+}
+
+/***************************************************************************
+ Function:  assign
+ Inputs:    int
+ Returns:   int (positive or negative)
+ Description:   indicates the sign of a variable within a given clause
+ ***************************************************************************/
+void assign(int variable, int value) {
+    assignment[variable] = value;
+    //variables.erase(variable);
+}
 
 /****** END OF FILE **********************************************************/
